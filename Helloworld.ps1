4fdec39a2d66d01a3d33adcf052b3128070e5a3b
AndyFul <andrzejula@o2.pl>
2024-10-11 20:01:36 +0200

    Add files via upload

---
+++ Helloworld.ps1
@@ -0,0 +1,21 @@
+# Filename: Hello.ps1
+Write-Host
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+$a=$ExecutionContext.SessionState.LanguageMode
+Write-Host $a
+
+start-sleep 65
+# end of script
